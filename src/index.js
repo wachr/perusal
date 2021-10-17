f97bc2d08da056f6cddc57ba3dd9856eb02f6c56
Ray Wach <wach.ray+git@gmail.com>
2021-10-17 12:57:33 -0700

    Add GitHub Action deploy to GitHub Pages
    Roughly following preactjs/preact#2178 as my guide.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import './style';
+import "./style";
+import baseroute from "./baseroute";
 
 export default function App() {
-	return (
-		<div>
-			<h1>Hello, World!</h1>
-			<img src="/assets/icon.svg" />
-		</div>
-	);
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <img src={`${baseroute}/assets/icon.svg`} />
+    </div>
+  );
 }
