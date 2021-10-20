44905dab1ebaf8e6c1cdda36b92d530b5ba1189c
Ray Wach <wach.ray+git@gmail.com>
2021-10-20 07:54:20 -0700

    Move tests into src directory

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -1,7 +1,7 @@
 import { h } from "preact";
 import { render, screen } from "@testing-library/preact";
 
-import App from "../../src/components/App";
+import App from "./App";
 
 describe(App.name, () => {
   it("renders", () => {
