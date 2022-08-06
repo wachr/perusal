da629657d7b23a9ff48f43ff225427cd3a89559f
Ray Wach <wach.ray+git@gmail.com>
2022-08-06 14:23:01 -0700

    Add dockerfile for dev support
    - Add conditional webpack config for Windows dev environment.

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:latest
+USER node
+SHELL [ "bash" ]
+ENV NODE_OPTIONS=--openssl-legacy-provider
+VOLUME /home/node/perusal
+WORKDIR /home/node/perusal
+EXPOSE 8080
+ENTRYPOINT ["npm", "run", "dev"]
--- preact.config.js
+++ preact.config.js
@@ -10,6 +10,11 @@ export default {
     const { plugin } = helpers.getPluginsByName(config, "DefinePlugin")[0];
     Object.assign(plugin.definitions, { "process.env.GITHUB_PAGES": ghEnv });
 
+    // If Windows, use polling to work around filesystem limitations
+    // https://webpack.js.org/configuration/watch/#watchoptionspoll
+    if (process.platform === 'win32')
+      config.devServer.watchOptions.poll = 1500;
+
     if (!config.resolve) config.resolve = {};
     if (!config.resolve.alias) config.resolve.alias = {};
     const prevAliases = config.resolve.alias;
