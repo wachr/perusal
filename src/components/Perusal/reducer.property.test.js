a5e1b2c49cb9fa85dd5e22ca076999abc6500c38
Ray Wach <wach.ray+git@gmail.com>
2022-12-24 11:15:57 -0800

    Incrementally add property tests

--- src/components/Perusal/reducer.property.test.js
+++ src/components/Perusal/reducer.property.test.js
@@ -2,17 +2,29 @@ import { assert, property } from "fast-check";
 import * as fc from "fast-check";
 
 import { isEmpty } from "./ops";
-import reduce, { DeleteString, withPath } from "./reducer";
+import reduce, {
+  DeleteString,
+  PromoteStringToArray,
+  withPath,
+} from "./reducer";
+
+function nonEmptyString() {
+  return fc.string({ minLength: 1 });
+}
+
+function nonEmptyArray(arb) {
+  return fc.uniqueArray(arb, { minLength: 1 });
+}
+
+function nonEmptyObject(arb) {
+  return fc.dictionary(nonEmptyString(), arb, { minKeys: 1 });
+}
 
 describe(reduce.name, () => {
   describe(DeleteString.name, () => {
-    const nonEmptyString = fc.string({ minLength: 1 });
-    const nonEmptyArray = (arb) => fc.uniqueArray(arb, { minLength: 1 });
-    const nonEmptyObject = (arb) =>
-      fc.dictionary(nonEmptyString, arb, { minKeys: 1 });
     it("on strings", () => {
       assert(
-        property(nonEmptyString, (str) => {
+        property(nonEmptyString(), (str) => {
           expect(reduce(str, DeleteString())).toStrictEqual({});
         })
       );
@@ -20,7 +32,7 @@ describe(reduce.name, () => {
 
     it("on arrays without a path", () => {
       assert(
-        property(nonEmptyArray(nonEmptyString), (arr) => {
+        property(nonEmptyArray(nonEmptyString()), (arr) => {
           expect(reduce(arr, DeleteString())).toBeUndefined();
         })
       );
@@ -36,7 +48,7 @@ describe(reduce.name, () => {
 
     it("on arrays of strings", () => {
       assert(
-        property(nonEmptyArray(nonEmptyString), fc.nat(), (arr, index) => {
+        property(nonEmptyArray(nonEmptyString()), fc.nat(), (arr, index) => {
           const nextState = reduce(arr, withPath(index)(DeleteString()));
           expect(nextState || []).not.toContain(arr[index]);
           arr.length === 1
@@ -48,6 +60,59 @@ describe(reduce.name, () => {
       );
     });
 
-    it.todo("on objects of strings");
+    it("on objects of strings", () => {
+      const arbPerusal = (arb) =>
+        nonEmptyObject(arb).chain((arbObj) =>
+          fc.record({
+            state: fc.constant(arbObj),
+            path: fc.oneof(...Object.keys(arbObj).map(fc.constant)),
+          })
+        );
+      assert(
+        property(arbPerusal(nonEmptyString()), ({ state, path }) => {
+          expect(state[path]).toBeDefined();
+          expect(reduce(state, withPath(path)(DeleteString()))).toBeUndefined();
+          expect(isEmpty(state[path])).toBe(true);
+        })
+      );
+    });
+  });
+
+  describe(PromoteStringToArray.name, () => {
+    it("on strings", () => {
+      assert(
+        property(nonEmptyString(), nonEmptyString(), (state, str) => {
+          const nextState = reduce(state, PromoteStringToArray(str));
+          expect(nextState).toStrictEqual([state, str]);
+        })
+      );
+    });
+
+    it("on arrays of strings", () => {
+      assert(
+        property(
+          nonEmptyArray(nonEmptyString()).chain((arr) =>
+            fc.record({
+              state: fc.constant(arr),
+              path: fc.option(fc.nat({ max: arr.length - 1 }).map(String)),
+            })
+          ),
+          nonEmptyString(),
+          ({ state, path }, str) => {
+            if (!path) {
+              const nextState = reduce(state, PromoteStringToArray(str));
+              expect(nextState).toStrictEqual(state);
+              return;
+            }
+            const nextState = reduce(
+              state,
+              withPath(path)(PromoteStringToArray(str))
+            );
+            expect(nextState).toBeUndefined();
+            expect(state[path]).toHaveLength(2);
+          }
+        )
+      );
+    });
   });
 });
